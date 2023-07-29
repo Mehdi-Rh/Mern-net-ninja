e38bb8c1f21210a21f06866b27537302bd0cd348
Mehdi Rahal <mehdi@lablabee.com>
2023-07-29 18:23:04 +0100

    Set the context

--- frontend-v1/src/components/WorkoutForm.js
+++ frontend-v1/src/components/WorkoutForm.js
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
 
 const WorkoutForm = () => {
+  const { dispatch } = useWorkoutsContext();
+
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
@@ -26,7 +29,7 @@ const WorkoutForm = () => {
       setTitle("");
       setLoad("");
       setReps("");
-      console.log("New workout added", json);
+      dispatch({ type: "CREATE_WORKOUT", payload: json });
     }
   };
   return (
