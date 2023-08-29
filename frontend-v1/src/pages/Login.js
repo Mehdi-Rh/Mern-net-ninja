11fb3cc40f31c8aede1cd81fdd95665481fecb46
Mehdi Rahal <mehdi@lablabee.com>
2023-08-29 08:43:02 +0100

    Auth #12 useLogin

--- frontend-v1/src/pages/Login.js
+++ frontend-v1/src/pages/Login.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
+import { useLogin } from "../hooks/useLogin";
 
 const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const { login, error, isLoading } = useLogin();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(email, password);
+    await login(email, password);
   };
 
   return (
@@ -16,7 +18,8 @@ const Login = () => {
       <input type="email" onChange={(e) => setEmail(e.target.value)} value={email} />
       <label>Password:</label>
       <input type="password" onChange={(e) => setPassword(e.target.value)} value={password} />
-      <button>Login</button>
+      <button disabled={isLoading}>Login</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
