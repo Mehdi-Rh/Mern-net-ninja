ec7ef02967cf44e9aeedf7a76bcbb0f73630529f
Mehdi Rahal <mehdi@lablabee.com>
2023-09-06 09:05:13 +0100

    #15 make authorized requests

--- frontend-v1/src/pages/Home.js
+++ frontend-v1/src/pages/Home.js
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useWorkoutsContext } from "../hooks/useWorkoutsContext";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 // components
 import WorkoutDetails from "../components/WorkoutDetails";
@@ -7,10 +8,17 @@ import WorkoutForm from "../components/WorkoutForm";
 
 const Home = () => {
   const { workouts, dispatch } = useWorkoutsContext();
+  const { user } = useAuthContext();
 
   useEffect(() => {
+    console.log("user", user);
+    if (!user) return;
     const fetchWorkouts = async () => {
-      const response = await fetch("/api/workouts");
+      const response = await fetch("/api/workouts", {
+        headers: {
+          Authorization: `Bearer ${user.token}`,
+        },
+      });
       const json = await response.json();
 
       if (response.ok) {
