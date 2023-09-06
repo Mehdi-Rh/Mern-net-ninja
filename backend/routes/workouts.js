ecd8e0e0f8f1ede2df40b1e9239d617cc8ff7ad3
Mehdi Rahal <mehdi@lablabee.com>
2023-09-06 08:42:16 +0100

    #14 Protecting API Routes

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -2,16 +2,26 @@ const express = require("express");
 const Workout = require("../models/workoutModel");
 const router = express.Router();
 
-const { createWorkout, getWorkout, getWorkouts, deleteWorkout, updateWorkout } = require("../controllers/workoutController");
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+  deleteWorkout,
+  updateWorkout,
+} = require("../controllers/workoutController");
+
+const requireAuth = require("../middleware/requireAuth");
+
+router.use(requireAuth);
 
 // Get all workouts
-router.get('/', getWorkouts)
+router.get("/", getWorkouts);
 
 // Get a specific workout
 router.get("/:id", getWorkout);
 
 // POST a new workout
-router.post('/',  createWorkout);
+router.post("/", createWorkout);
 
 // Delete a workout
 router.delete("/:id", deleteWorkout);
